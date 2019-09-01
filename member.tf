c55c256e39f7fd55d864264ec993d6438392d702
Alex Harvey <alex.harvey@cmd.com.au>
2019-09-01 18:07:51 +1000

    Minor simplification in member.tf

--- member.tf
+++ member.tf
@@ -1,5 +1,5 @@
 resource "aws_guardduty_invite_accepter" "member_accepter" {
-  count             = var.is_guardduty_member && length(var.master_account_id) > 0 ? 1 : 0
+  count             = var.is_guardduty_member ? 1 : 0
   detector_id       = aws_guardduty_detector.detector.id
   master_account_id = var.master_account_id
 }
