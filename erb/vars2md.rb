029e5433a0969d2c9f42e34925198aca7ba2d5d4
Alex Harvey <alex.harvey@cmd.com.au>
2019-09-01 20:36:43 +1000

    Create vars2md.rb as a separate script

---
+++ erb/vars2md.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+file = ARGV[0] ; result = '' ; data = {}
+
+File.readlines(file).each do |line|
+  if line =~ /variable/
+    name = line.split('"')[1]
+    data = {:name=>name}
+  end
+
+  if line =~ /type/
+    data[:type] = line =~ /object/ ? 'object' : line.split(' ')[2]
+  end
+
+  if line =~ /description/
+    data[:description] = line.split('"')[1]
+  end
+
+  if line =~ /default/
+    data[:default] = line.split('=')[1].gsub(/"/,'').gsub(/^ +/,'').chomp
+  end
+
+  if line =~ /^}/
+    data[:required] = data[:default] == '' ? 'Yes' : 'No'
+    string = \
+      "|" + data[:name] + \
+      "|" + data[:description] + \
+      "|" + data[:type] + \
+      "|" + data[:default] + \
+      "|" + data[:required] + "|\n" 
+    result += string
+  end
+end
+
+puts result
