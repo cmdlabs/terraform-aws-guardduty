348e69b2f1a19e862078fee6cf8b9bab2a2571b3
Alex Harvey <alex.harvey@cmd.com.au>
2019-09-08 18:18:58 +1000

    Minor clean up in spec/simple_spec.rb

--- examples/spec/simple_spec.rb
+++ examples/spec/simple_spec.rb
@@ -10,10 +10,18 @@
 # https://github.com/inspec/inspec-aws/tree/master/docs/resources
 # and would need to be contributed to that project.
 
-Dir.chdir("./master_simple")
+def terraform(action, dir)
+  Dir.chdir(dir)
+  exit_status = system("terraform #{action} -auto-approve")
+  Dir.chdir("..")
+  return exit_status
+end
 
-describe command("terraform apply -auto-approve") do
-  its(:exit_status) { should eq 0 }
+describe "terraform apply" do
+  it "should return exit status 0" do
+    exit_status = terraform("apply", "./master_simple")
+    expect(exit_status).to eq 0
+  end
 end
 
 detector_id = aws_guardduty_detectors[0]
@@ -38,6 +46,9 @@ describe aws_guardduty_threatintelset(detector_id: detector_id) do
   its(:location) { should match /threatintelset.txt/ }
 end
 
-describe command("terraform destroy -auto-approve") do
-  its(:exit_status) { should eq 0 }
+describe "terraform destroy" do
+  it "should return exit status 0" do
+    exit_status = terraform("destroy", "./master_simple")
+    expect(exit_status).to eq 0
+  end
 end
